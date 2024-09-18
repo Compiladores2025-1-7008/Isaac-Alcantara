25ece0784ca44f605fdf1aabcb062bcc283f7f93
IsaacAE <alcantara_estrada_kevin@ciencias.unam.mx>
2024-09-17 23:11:45 -0600

    Comentando código

--- LectorArchivo.java
+++ LectorArchivo.java
@@ -1,43 +1,55 @@
 import java.io.FileReader;
 import java.io.IOException;
 
+//Clase utilizada para leer el contenido del archivo caracter por caracter
 public class LectorArchivo {
-    private static String yytext = ""; // Lexema actual
+    private static String yytext = ""; // Cadena más larga que actúa como lexema actual
 
+    //Función utilizada para leer el siguiente caracter del archivo hasta hallar el final del mismo
     public static char leerCaracter(FileReader fileReader) throws IOException {
         int charCode = fileReader.read();
         return (charCode == -1) ? '\0' : (char) charCode; // Fin de archivo o el siguiente carácter
     }
 
+    //Función que almacena el la cadena coincidente con alguna expresión del lenguaje más larga (lexema) actual
     public static void yylex(FileReader fileReader) throws IOException {
-        yytext = "";        // Inicializamos yytext
-        Automata.asignarEstadoActual(0);    // Inicializamos el estado actual
-
-        char c = leerCaracter(fileReader); // Obtenemos el primer carácter
-
-        while (c != '\0') {  // Mientras no lleguemos al final del archivo
+        yytext = "";        // Inicializamos yytext (almacena el lexema actual)
+        Automata.asignarEstadoActual(0);    // Inicializamos el estado actual en 0 (estado inicial del autómata)
+    
+        char c = leerCaracter(fileReader); // Leemos el primer carácter del archivo
+    
+        // Este proceso se ejecuta mientras no lleguemos al final del archivo 
+        while (c != '\0') {  
+            // Obtenemos el siguiente estado según el estado actual y el caracter leído
             int siguienteEstado = Automata.obtenerSiguienteEstado(Automata.obtenerEstadoActual(), c);
-
+    
+            // Si se encuentra un siguiente estado válido, se transiciona al mismo y se actualiza el lexema
             if (siguienteEstado != -1) {
-                Automata.asignarEstadoActual(siguienteEstado);
-                yytext += c;
-                c = leerCaracter(fileReader); // Obtener el siguiente carácter
+                Automata.asignarEstadoActual(siguienteEstado);  
+                yytext += c;  // Agregamos el carácter al lexema actual
+                c = leerCaracter(fileReader);  
             } else {
+               
                 if (Automata.esEstadoDeAceptacion(Automata.obtenerEstadoActual())) {
-                    if (Automata.obtenerEstadoActual() != 4) {
-                        ManejadorTokens.obtenerToken(Automata.obtenerEstadoActual(), yytext); // Si es estado de aceptación, obtenemos el token
+                    //Mostramos el token en pantalla si no es un pesacio
+                    if (Automata.obtenerEstadoActual() != 4) { 
+                        ManejadorTokens.obtenerToken(Automata.obtenerEstadoActual(), yytext);  
                     }
-                    yytext = ""; // Reiniciar yytext después de obtener el token
-                    Automata.asignarEstadoActual(0); // Volver al estado inicial
-
+                    //Reiniciamos el proceso volviendo al estado inicial y vaciando el lexema que se había almacenado
+                    yytext = ""; 
+                    Automata.asignarEstadoActual(0);  
                 } else {
-                    ManejadorTokens.reportarError(c); // Reportar error léxico
-                    yytext = ""; // Reiniciar yytext
-                    Automata.asignarEstadoActual(0); // Volver al estado inicial
-                    c = leerCaracter(fileReader); // Obtener el siguiente carácter
+                    // Si el estado actual no es un estado de aceptación, reportamos un error léxico
+                    ManejadorTokens.reportarError(c);  
+                     //Reiniciamos el proceso volviendo al estado inicial y vaciando el lexema que se había almacenado
+                    yytext = ""; 
+                    Automata.asignarEstadoActual(0);  
+                    c = leerCaracter(fileReader);  
                 }
             }
         }
-        fileReader.close();
+    
+        fileReader.close();  // Cerramos el archivo después de terminar la lectura
     }
+    
 }
