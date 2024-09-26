6da02e3bb720b86ca963e1a03e65e240118d8258
IsaacAE <alcantara_estrada_kevin@ciencias.unam.mx>
2024-09-25 18:45:23 -0600

    Se modificó la el archivo que contiene las clases Léxicas

--- src/main/java/ClaseLexica.java
+++ src/main/java/ClaseLexica.java
@@ -2,5 +2,20 @@ package main.java;
 
 public class ClaseLexica {
     public static final int INT = 1;
+    public static final int FLOAT = 2;
+    public static final int IF = 3;
+    public static final int ELSE = 4;
+    public static final int WHILE = 5;
+    public static final int LPAR = 6;
+    public static final int RPAR = 7;
+    public static final int ASIG = 8;
+    public static final int ADD = 9;
+    public static final int MINUS = 10;
+    public static final int TIMES = 11;
+    public static final int DIV = 12;
+    public static final int PYC = 13;
+    public static final int COMA = 14;
+    public static final int ID = 15;
+    public static final int NUM = 15;
     //Agregar los simbolos terminales, el valor es a tu elección.
 }
